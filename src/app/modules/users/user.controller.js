c04a69039547dcfa89e49bf5794b5c6bac30d550
aliibnemasud <mdmasudrony@gmail.com>
2023-09-09 18:21:05 +0600

    crud oparation are working fine

--- src/app/modules/users/user.controller.js
+++ src/app/modules/users/user.controller.js
@@ -1,30 +1,96 @@
 const connection = require("../../db");
 
 const getAllUser = async (req, res) => {
-  connection.query("select * from users", (err, result) => {
-    if (err) {
-      console.log(err);
-    } else {
-      res.json(result);
-    }
-  });
-};
-
-const postUser = (req, res) => {
-  connection.query(
-    "INSERT INTO users set ?",
-    req.body,
-    (err, result, fields) => {
+  try {
+    connection.query("select * from users", (err, result) => {
       if (err) {
         console.log(err);
       } else {
         res.json(result);
       }
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const postUser = (req, res) => {
+  try {
+    connection.query(
+      "INSERT INTO users set ?",
+      req.body,
+      (err, result, fields) => {
+        if (err) {
+          console.log(err);
+        } else {
+          res.json(result);
+        }
+      }
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const updateUser = (req, res) => {
+  try {
+    const id = req.params.id;
+    const sql = `UPDATE users SET ? WHERE id= ?`;
+    const updatedData = req.body;
+    if (id && updatedData) {
+      connection.query(sql, [updatedData, id], (err, result) => {
+        if (err) {
+          console.log(err);
+        } else {
+          res.json(result);
+        }
+      });
+    } else {
+      res.json("Please Provide Id and Data!");
     }
-  );  
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getSingleUser = (req, res) => {
+  try {
+    const id = req.params.id;
+    const sql = `SELECT * FROM users WHERE id=${id}`;
+    connection.query(sql, (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.json(result);
+      }
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const deleteUser = (req, res) => {
+  try {
+    const id = req.params.id;
+    if (id) {    
+      const sql = `DELETE FROM users WHERE id=${id}`;
+      connection.query(sql, (err, result) => {
+        if (err) {
+          console.log(err);
+        } else {
+          res.json(result);
+        }
+      });
+    }
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 module.exports = {
   getAllUser,
   postUser,
+  getSingleUser,
+  updateUser,
+  deleteUser
 };
--- src/app/modules/users/user.route.js
+++ src/app/modules/users/user.route.js
@@ -1,14 +1,15 @@
 const express = require("express");
-const { getAllUser, postUser } = require("./user.controller");
+const { getAllUser, postUser, updateUser, getSingleUser, deleteUser } = require("./user.controller");
 const router = express.Router();
 
 router
   .route("/")
-
   .get(getAllUser)
-
-  .post(postUser);
+  .post(postUser)
 
 router.route("/addUser").post(postUser);
+router.route("/updateUser/:id").patch(updateUser)
+router.route("/getUser/:id").get(getSingleUser)
+router.route("/deleteUser/:id").delete(deleteUser)
 
 module.exports = router;
