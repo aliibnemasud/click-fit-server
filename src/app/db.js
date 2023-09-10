9be526fd3e58d6072102f8fdb20dde65038605dd
aliibnemasud <mdmasudrony@gmail.com>
2023-09-10 13:47:39 +0600

    working on the namecheep

--- src/app/db.js
+++ src/app/db.js
@@ -1,10 +1,10 @@
-const mysql      = require('mysql');
+const mysql = require("mysql");
 
 const connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'taqirwax_click_fit',
-  password : '^Dwu#tKd*S7*R4^%',
-  database : 'taqirwax_click_fit'
+  host: "localhost",
+  user: "taqirwax_click_fit",
+  password: "^Dwu#tKd*S7*R4^%",
+  database: "taqirwax_click_fit"
 });
 
 module.exports = connection;
